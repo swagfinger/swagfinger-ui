5a59d0baae11b345f4d3f10bb10f98d0e7d21df4
swagfinger <swagfinger@gmail.com>
2023-03-08 07:43:57 +0800

    Button

--- src/index.production.js
+++ src/index.production.js
@@ -1,4 +1,5 @@
 import {Accordion, AccordionItem, AccordionExample} from './components/accordion';
 import {Icon, IconExample} from './components/icon';
+import {Button, ButtonExample} from './components/button';
 
-export {Accordion, AccordionItem, AccordionExample, Icon, IconExample}
+export {Accordion, AccordionItem, AccordionExample, Icon, IconExample, Button, ButtonExample}
